991b0a64255a78ec43371cdc73157d73cc1ba5ca
Belesova <belesova@technokratos.com>
2023-03-05 22:30:27 +0300

    add models

--- web/models.py
+++ web/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=256)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    created_At = models.DateTimeField
+    counts_likes = models.IntegerField(default=0)
+    image = models.ImageField(upload_to='time_slots/', null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
